55bb0611ed1de9c5202cd1b61a3b8711cb673bd8
nathaliaveneziano <nathaliaveneziano@icloud.com>
2023-11-22 17:38:41 -0300

    static login page finished

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,43 @@
+import { Logo } from '../../components/Logo';
+import { useState } from 'react';
+import './login.css';
+
+
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleLogin(e) {
+    e.preventDefault();
+
+    if (email === '' || password === '') {
+      alert('Preencha todos os campos!');
+      return;
+    }
+
+    console.log(email);
+    console.log(password);
+  }
+
   return (
-    <div>
-      <h1>Página Login</h1>
+    <div className="login">
+      <Logo />
+      <form className="form" onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Digite seu email..."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="********"
+          autoComplete="on"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Acessar</button>
+      </form>
     </div>
   );
 }
