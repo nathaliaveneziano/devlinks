c711a8f1ff098717cbe044502ed0bfc9b5e86a97
nathaliaveneziano <nathaliaveneziano@icloud.com>
2023-11-28 22:25:35 -0300

    protected route admin

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,19 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Admin from './pages/Admin';
 import Error from './pages/Error';
+import Private from './routes/Private';
 
 const router = createBrowserRouter([
   { path: '/', element: <Home /> },
   { path: '/login', element: <Login /> },
-  { path: '/admin', element: <Admin /> },
+  {
+    path: '/admin',
+    element: (
+      <Private>
+        <Admin />
+      </Private>
+    ),
+  },
   { path: '*', element: <Error /> },
 ]);
 
